db329ae0bdae017ae3595beb65188e1f2ca3c37b
autosquash <49131885+autosquash@users.noreply.github.com>
2025-03-09 22:59:24 +0100

    extract Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { invoke } from '@tauri-apps/api/core'
 import { useEffect, useState } from 'react'
 import './App.css'
-
-interface Movie {
-  title: string
-  view_date?: string | null
-  image_url?: string | null
-}
+import Card, { Movie } from './Card'
 
 const colors = [
   'orange',
@@ -16,13 +11,6 @@ const colors = [
   'coral',
   'LightSeaGreen',
 ]
-
-const parseViewDate = (viewDate: string | null | undefined) => {
-  if (!viewDate || viewDate === 'unknown') {
-    return ''
-  }
-  return `(vista el ${viewDate!.split('-').reverse().join('-')})`
-}
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([])
@@ -34,29 +22,13 @@ export default function App() {
     })
   }, [])
 
-  const getViewDateString = (movie: Movie) => parseViewDate(movie.view_date)
-
   return (
     <div className="container">
       <h1>Ya hemos visto como mínimo {movies.length} películas</h1>
       <ul>
         {movies.map((movie, index) => (
           <li key={index}>
-            <div
-              className="movie-card"
-              style={{ backgroundColor: colors[index % colors.length] }}
-            >
-              <span>
-                <strong>{movie.title}</strong> {getViewDateString(movie)}
-              </span>
-              {movie.image_url && (
-                <img
-                  src={`data/${movie.image_url}`}
-                  alt={movie.title}
-                  className="movie-thumbnail"
-                />
-              )}
-            </div>
+            <Card movie={movie} color={colors[index % colors.length]} />
           </li>
         ))}
       </ul>
