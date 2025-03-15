256642baa91d864cc3d2bacece84dfc6457b12a3
autosquash <49131885+autosquash@users.noreply.github.com>
2025-03-15 09:43:46 +0100

    show films

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,28 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::PathBuf;
+use tauri::command;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Movie {
+    title: String,
+    view_date: String,
+}
+
+#[command]
+fn get_movies() -> Vec<Movie> {
+    let path = PathBuf::from("../data/movies.json");
+    let data = fs::read_to_string(path).unwrap_or("[]".to_string());
+    println!("{:#?}", data);
+    serde_json::from_str(&data).unwrap_or_else(|_| vec![])
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_movies])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import "./App.css";
+import { invoke } from '@tauri-apps/api/core'
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-
-  return (
-    <main className="container">
-
-    </main>
-  );
+interface Movie {
+  title: string
+  year: number
+  genre: string
 }
 
-export default App;
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([])
+
+  useEffect(() => {
+    invoke<Movie[]>('get_movies').then((fetchedMovies) => {
+      console.log(fetchedMovies)
+      setMovies(fetchedMovies)
+    })
+  }, [])
+
+  return (
+    <div className="container">
+      <h1>Listado de Películas</h1>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>
+            <strong>{movie.title}</strong> ({movie.year}) - {movie.genre}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
