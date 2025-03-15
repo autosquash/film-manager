f35d6c0a8b3d3c2c48b0df6285da3bfd58f04beb
autosquash <49131885+autosquash@users.noreply.github.com>
2025-03-15 15:58:15 +0100

    read and save movies with the right format

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use serde::{Deserialize, Serialize};
-use std::fs;
+use serde_json::to_writer_pretty;
+use std::fs::{self, File};
 use std::path::PathBuf;
 use tauri::command;
 use uuid::Uuid;
@@ -8,16 +9,37 @@ use uuid::Uuid;
 #[derive(Serialize, Deserialize, Debug)]
 struct Movie {
     title: String,
-    view_date: String,
+    view_date: Option<String>,
+    premiere_date: Option<String>,
+    movie_url: Option<String>,
     image_url: Option<String>,
     id: Option<Uuid>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    movies: Vec<Movie>,
+}
+
+const PATH: &str = "../data/movies.json";
+
 #[command]
 fn get_movies() -> Vec<Movie> {
-    let path = PathBuf::from("../data/movies.json");
-    let data = fs::read_to_string(path).unwrap_or("[]".to_string());
-    let movies = serde_json::from_str(&data).unwrap_or_else(|_| vec![]);
+    let path = PathBuf::from(PATH);
+    let data = fs::read_to_string(path).unwrap();
+    let movies_result: Result<Vec<Movie>, serde_json::Error> = serde_json::from_str(&data);
+    let movies: Vec<Movie> = match movies_result {
+        Ok(_) => movies_result.unwrap(),
+        Err(_) => {
+            let data: Data = serde_json::from_str(&data).unwrap();
+            data.movies
+        }
+    };
+
+    if movies.len() == 0 {
+        return vec![];
+    }
+
     let new_movies = movies
         .into_iter()
         .map(|movie: Movie| match movie.id {
@@ -25,12 +47,23 @@ fn get_movies() -> Vec<Movie> {
             None => Movie {
                 title: movie.title,
                 view_date: movie.view_date,
+                premiere_date: movie.premiere_date,
+                movie_url: movie.movie_url,
                 image_url: movie.image_url,
                 id: Some(Uuid::new_v4()),
             },
         })
         .collect();
-    new_movies
+
+    let data = Data { movies: new_movies };
+
+    let json = serde_json::to_string_pretty(&data).unwrap();
+    println!("{}", json);
+
+    let file = File::create("../data/movies-output.json").unwrap();
+    to_writer_pretty(file, &data).unwrap();
+
+    data.movies
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
