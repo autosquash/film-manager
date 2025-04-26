e65ce980e58d038762f9238d9712c5948b6f4444
autosquash <49131885+autosquash@users.noreply.github.com>
2025-04-26 20:19:38 +0200

    format

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -161,19 +161,21 @@ function normalizeDate(date: string): string | null {
   if (year.length !== 4) {
     throw new Error(`Wrong length of year string: ${year.length}`)
   }
+
   if (day.length === 1) {
     day = '0' + day
   }
-  if (month.length === 1) {
-    month = '0' + month
-  }
-
   if (day.length !== 2) {
     throw new Error('Wrong length of day string')
+  }
+
+  if (month.length === 1) {
+    month = '0' + month
   }
   if (month.length !== 2) {
     throw new Error('Wrong length of month string')
   }
+
   if (parseInt(day) > 31 || parseInt(month) > 12) {
     throw new Error('Invalid value for date')
   }
